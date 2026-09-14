f0ed3d314113bedf0f4f3ade444e409aa7b70ee2
punkmyduck <punkmyduck@mail.ru>
2026-09-14 22:11:32 +0300

    feat: зависимости зарегистрированы в IoC

--- B2BOrderBridge/OrderBridge/OrderBridge.Infrastructure/DependencyInjection.cs
+++ B2BOrderBridge/OrderBridge/OrderBridge.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OrderBridge.Application.Features.Orders.Repositories;
 using OrderBridge.Infrastructure.Persistence;
 using OrderBridge.Infrastructure.Services;
 using Shared.Application.Persistence;
@@ -9,6 +11,8 @@ namespace OrderBridge.Infrastructure;
 
 public static class DependencyInjection
 {
+    private const string OrderBridgeConnectionString = "OrderBridge";
+
     public static IServiceCollection AddServices(this IServiceCollection services)
     {
         services.AddSingleton<IDateTimeOffsetProvider, DateTimeOffsetProvider>();
@@ -22,6 +26,31 @@ public static class DependencyInjection
         services.AddScoped<IUnitOfWork>(provider => provider.GetRequiredService<OrderBridgeDbContext>());
         services.AddScoped(typeof(IRepository<,>), typeof(EfRepository<,>));
         services.AddScoped(typeof(IReadRepository<,>), typeof(EfRepository<,>));
+        services.AddScoped<IOrderReadRepository, OrderReadRepository>();
         return services;
+    }
+
+    public static IServiceCollection AddPersistence(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        ArgumentNullException.ThrowIfNull(configuration);
+
+        var connectionString = configuration.GetConnectionString(OrderBridgeConnectionString)
+            ?? throw new InvalidOperationException(
+                $"Connection string '{OrderBridgeConnectionString}' is not configured.");
+
+        return services.AddPersistence(connectionString);
+    }
+
+    public static async Task ApplyPersistenceMigrationsAsync(
+        this IServiceProvider serviceProvider,
+        CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(serviceProvider);
+
+        await using var scope = serviceProvider.CreateAsyncScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<OrderBridgeDbContext>();
+        await dbContext.Database.MigrateAsync(cancellationToken);
     }
 }
