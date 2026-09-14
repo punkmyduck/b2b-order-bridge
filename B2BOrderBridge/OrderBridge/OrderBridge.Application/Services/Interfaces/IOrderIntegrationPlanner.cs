fbb3a5752011f7b5b1196bbbe6e7a34803563b41
punkmyduck <punkmyduck@mail.ru>
2026-09-14 22:06:53 +0300

    feat: добавлен класс для создания интеграций заказа

---
+++ B2BOrderBridge/OrderBridge/OrderBridge.Application/Services/Interfaces/IOrderIntegrationPlanner.cs
@@ -0,0 +1,9 @@
+﻿using OrderBridge.Application.ValueObjects;
+using OrderBridge.Domain.Models;
+
+namespace OrderBridge.Application.Services.Interfaces;
+
+public interface IOrderIntegrationPlanner
+{
+    IReadOnlyList<OrderIntegration> Create(Order order, OrderProcessingScenario scenario, DateTimeOffset now);
+}
