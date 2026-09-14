694d09fd141a9d688e79dbd030d115ad5b1ae40c
punkmyduck <punkmyduck@mail.ru>
2026-09-14 22:15:57 +0300

    build: добавлены настройки для dev-среды. feat: добавлен класс-реквест для создания заказов

---
+++ B2BOrderBridge/OrderBridge/OrderBridge.Presentation/Requests/CreateOrderRequest.cs
@@ -0,0 +1,10 @@
+﻿using OrderBridge.Application.ValueObjects;
+
+namespace OrderBridge.Presentation.Requests;
+
+public sealed record CreateOrderRequest(
+    string SourceSystem,
+    string ExternalOrderId,
+    CustomerSnapshotDto Customer,
+    string Currency,
+    IReadOnlyCollection<OrderLineDto> Lines);
